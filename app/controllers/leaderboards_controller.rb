5f7ee05e15ebcc53c60cc7e5fb0797d829fea1cf
Daniel Mantilla <dan@appcabinet.com>
2016-01-10 13:39:40 -0500

    Leaderboard view

---
+++ app/controllers/leaderboards_controller.rb
@@ -0,0 +1,6 @@
+class LeaderboardsController < ApplicationController
+  before_action :authenticate_user!
+
+  def show
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   resources :scores, only: [:index, :new, :create]
 
+  resource :leaderboard, only: :show
+
   devise_for :users
 
   root to: "home#index"
