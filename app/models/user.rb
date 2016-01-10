972fc5faafe9d07d2e33c4e2b315625e9d0f3737
Daniel Mantilla <dan@appcabinet.com>
2016-01-10 13:53:39 -0500

    User#score_history

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable, :trackable, :validatable
 
   has_many :scores, foreign_key: 'created_by_id'
+  has_many :opponent_scores, foreign_key: 'opponent_id', class_name: 'Score'
 
   scope    :by_final_score, -> { order(score: :desc) }
 
@@ -9,4 +10,7 @@ class User < ActiveRecord::Base
     User.where.not(id: self.id)
   end
 
+  def score_history
+    scores + opponent_scores
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,11 +3,16 @@ require 'rails_helper'
 describe User do
   context 'associations' do
     specify { expect(subject).to have_many(:scores).with_foreign_key('created_by_id') }
+    specify { expect(subject).to have_many(:opponent_scores).with_foreign_key('opponent_id').class_name('Score') }
   end
 
   context '#opponents' do
     let(:me) { User.new(id: 30) }
 
     specify { expect(me.opponents).to eq(User.where.not(id: 30)) }
+  end
+
+  context '#score_history' do
+    specify { expect(subject.score_history).to eq(subject.scores + subject.opponent_scores)}
   end
 end
