b6fd5235f7ec792d4f2f70bae2a50aa98896ee5a
Daniel Mantilla <dan@appcabinet.com>
2016-01-11 22:33:21 -0500

    changing the landing page to the leaderboard

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,5 @@ Rails.application.routes.draw do
 
   devise_for :users
 
-  root to: "home#index"
-
-  get '/history', to: 'home#history'
-  get '/log',     to: 'home#log'
+  root to: "leaderboards#show"
 end
